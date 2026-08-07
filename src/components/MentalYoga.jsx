aad8d7c8f900d20961f247f764821979094dd32e
ADITYA_SADEWALE <adityasadewale@gmail.com>
2026-08-07 08:52:21 -0700

    Update MentalYoga.jsx

--- src/components/MentalYoga.jsx
+++ src/components/MentalYoga.jsx
@@ -95,6 +95,7 @@ export default function MentalYoga() {
           ))}
         </div>
 
+
         
         {/* Schedule Content */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 relative">
